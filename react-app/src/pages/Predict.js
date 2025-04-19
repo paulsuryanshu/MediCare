089d2908598efcae193d55b51c757cc10755fde0
Suryanshu Paul <suryanshupaul1990@gmail.com>
2025-04-19 13:37:07 +0530

    Update Predict.js

--- react-app/src/pages/Predict.js
+++ react-app/src/pages/Predict.js
@@ -51,7 +51,7 @@ const Predict = () => {
         formData.append("custom_symptoms", customSymptoms.join(","));
 
         try {
-            const response = await fetch("http://localhost:5000/predict", {
+            const response = await fetch("https://medicare-u2v5.onrender.com", {
                 method: "POST",
                 body: formData,
             });
